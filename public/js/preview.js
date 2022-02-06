fc64460c2b530f393bd166a37ebb9bde833fc7e1
hopollo <hopollotv@gmail.com>
2022-02-06 16:52:46 +0300

    Reworking again & better

--- public/js/preview.js
+++ public/js/preview.js
@@ -1,13 +1,42 @@
-document.querySelector('#search').onchange = async () => {
-  const searchInput = document.querySelector('#search').value;
-  const imgEl = document.querySelector('.imgResult');
-  const jsonEl = document.querySelector('.jsonResult');
+document.querySelector("form").onsubmit = async (e) => {
+  e.preventDefault();
 
-  const jsonResults = await fetch(`/v1/name/${searchInput}`).then(res => res.json());
-  jsonEl.innerHTML =  `<pre>${JSON.stringify(jsonResults, undefined, 2)}</pre>`;
+  const searchInput = document.querySelector("#search").value;
+  const imgEl = document.querySelector(".imgResult");
+  const jsonEl = document.querySelector(".jsonResult");
 
-  const mobData = await fetch(`https://hopollocors.herokuapp.com/https://solomonk.fr/fr/monstre/${jsonResults.id}`).then(res => res.text());
-  console.log(mobData);
-  const img = mobData.split('https://solomonk.fr/img/monsters/artworks/')[1].split('.svg')[0];
-  imgEl.setAttribute('src', `https://solomonk.fr/img/monsters/artworks/${img}`);
+  try {
+    imgEl.setAttribute("alt", "Loading...");
+
+    const jsonResults = await fetch(`/v1/name/${searchInput}`).then((res) =>
+      res.json()
+    );
+
+    console.log(searchInput);
+
+    jsonEl.innerHTML = `<pre>${JSON.stringify(
+      jsonResults,
+      undefined,
+      2
+    )}</pre>`;
+
+    const options = {
+      "headers": {
+        "X-Requested-With": "XMLHttpRequest"
+      }
+    }
+
+    const mobData = await fetch(`https://hopollocors.herokuapp.com/https://solomonk.fr/ajax/select_monster.php?lang=fr&Q=10&O=0&T=all&I=${jsonResults.id}&CS%5BbestiaryCollapseSpells%5D=true&CS%5BbestiaryCollapseSubareas%5D=true&CS%5BbestiaryCollapseDrops%5D=true&CS%5BbestiaryCollapseDropsTemporis%5D=true`, options).then(res => res.json());
+    console.log(mobData);
+
+    const img = JSON.stringify(mobData.html).split('https://solomonk.fr/img/monsters/artworks/')[1].split('.svg')[0];
+
+    imgEl.setAttribute("alt", jsonResults.name);
+    imgEl.setAttribute(
+      "src",
+      `https://solomonk.fr/img/monsters/artworks/${img}`
+    );
+  } catch (e) {
+    console.error(e);
+  }
 };
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
 const { readFile, writeFile } = require("fs");
 const fetch = require("node-fetch");
@@ -10,7 +10,7 @@ const filePath = "./monsters.json";
 
 let updating = false;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.get("/v1/id/:id", (req, res) => {
   const id = req.params.id;
@@ -24,18 +24,18 @@ app.get("/v1/id/:id", (req, res) => {
 });
 
 app.get("/v1/name/:name", (req, res) => {
-  const name = req.params.name.toLowerCase()
-  .split(' ')
-  .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-  .join(' ');
-
+  const name = req.params.name
+    .toLowerCase()
+    .split(" ")
+    .join(" ");
+  
   readFile(filePath, (err, data) => {
     if (err) return console.error(err);
 
     const json = JSON.parse(data);
 
     for (const mob in json) {
-      if (json[mob] == name) {
+      if (json[mob].toLowerCase() == name) {
         res.status(200).json({ id: mob, name: json[mob] });
       }
     }
@@ -43,7 +43,7 @@ app.get("/v1/name/:name", (req, res) => {
 });
 
 app.get("/v1/monsters", (req, res) => {
-  readFile(filePath, 'utf-8', (err, data) => {
+  readFile(filePath, "utf-8", (err, data) => {
     if (err) return res.status(500);
     res.status(200).json(JSON.parse(data));
   });
@@ -52,7 +52,7 @@ app.get("/v1/monsters", (req, res) => {
 app.get("/v1/update/:code", async (req, res) => {
   const code = req.params.code;
 
-  if (code !== process.env.CODE) return console.error('Access Denied.');
+  if (code !== process.env.CODE) return console.error("Access Denied.");
 
   if (updating)
     return (
@@ -65,36 +65,54 @@ app.get("/v1/update/:code", async (req, res) => {
   updating = true;
 
   for (let i = 0, len = 10000; i < len; i++) {
-    console.log(`${i}/${len}`);
-    // Getting all good ids from Ankama DB
-    const foundID = await fetch( `https://static.ankama.com/dofus/www/game/monsters/200/${i}.png`);
-    if (foundID.status !== 200) continue;
+    console.log(`${i}/${len} | (${Math.round((i/len)*100)}%)`);
+    try {
+      // Getting all good ids from Ankama DB
+      const foundID = await fetch(
+        `https://static.ankama.com/dofus/www/game/monsters/200/${i}.png`
+      );
+      if (foundID.status !== 200) continue;
 
-    // Getting matching names from Solomonk DB
-    const data = await fetch(`https://solomonk.fr/fr/monstre/${i}`).then((res) => res.text());
+      // Getting matching names from Solomonk DB
+      const options = {
+        headers: {
+          "X-Requested-With": "XMLHttpRequest",
+        },
+      };
 
-    const name = data
-      .split("text-sololightbeige")[1]
-      .split(">")[1]
-      .split("<")[0];
+      const mobData = await fetch(
+        `https://hopollocors.herokuapp.com/https://solomonk.fr/ajax/select_monster.php?lang=fr&Q=10&O=0&T=all&I=${i}&CS%5BbestiaryCollapseSpells%5D=true&CS%5BbestiaryCollapseSubareas%5D=true&CS%5BbestiaryCollapseDrops%5D=true&CS%5BbestiaryCollapseDropsTemporis%5D=true`,
+        options
+      ).then((res) => res.json());
+      if (mobData.status !== 200) continue;
 
-    readFile(filePath, "utf-8", (err, data) => {
-      if (err) return console.error(err);
+      const name = JSON.stringify(mobData.html)
+        .split("text-sololightbeige")[1]
+        .split(">")[1]
+        .split("<")[0];
 
-      const json = JSON.parse(data);
-      json[i] = name;
+      console.log(name);
 
-      writeFile(filePath, JSON.stringify(json), (err) => {
+      readFile(filePath, "utf-8", (err, data) => {
         if (err) return console.error(err);
+
+        const json = JSON.parse(data);
+        json[i] = name;
+
+        writeFile(filePath, JSON.stringify(json), (err) => {
+          if (err) return console.error(err);
+        });
       });
-    });
+    } catch (e) {
+      console.log("@update", e);
+    }
 
     updating = false;
   }
 });
 
 app.get("/", (req, res) => {
-  res.send('index.html');
+  res.send("index.html");
 });
 
 app.listen(PORT, () => console.log(`Running : http://localhost:${PORT}/`));
